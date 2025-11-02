83791a4cb146e4a6a03e81ad4b4a9d6cdd877449
Tony Ebel <tonyebel123@gmail.com>
2025-11-01 19:10:09 -0500

    setup docker-credential-ecr-login

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -103,7 +103,7 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route" "private_internet_route" {
   route_table_id         = aws_route_table.private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.nat.id
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
 resource "aws_route_table_association" "private_subnet_association" {
